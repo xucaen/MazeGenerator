6c9cd55fc3204383778ca607862637d787f2c841
xucaen <xucaen@gmail.com>
2026-04-20 13:52:33 -0500

    #res://scripts/npc_area_0.gd

--- MazeGenerator/MazeGameBuilder.cs
+++ MazeGenerator/MazeGameBuilder.cs
@@ -35,11 +35,11 @@ namespace MazeGenerator
             final.AppendLine();
 
             // BASE RESOURCES
-            final.AppendLine(@"[ext_resource type=""PackedScene"" path=""res://Vehicles/space_fighter_3w_lights.tscn"" id = ""space_fighter""]");
-            final.AppendLine(@"[ext_resource type=""PackedScene"" path=""res://Vehicles/cycle_2.tscn"" id = ""cycle""]");
-            final.AppendLine(@"[ext_resource type=""PackedScene"" path=""res://Vehicles/propeller_pack_2.tscn"" id = ""prop_pack""]");
-            final.AppendLine(@"[ext_resource type=""PackedScene"" path=""res://player.tscn"" id=""player""]");
-            final.AppendLine(@"[ext_resource type=""PackedScene"" path=""res://world.tscn"" id=""world""]");
+            //final.AppendLine(@"[ext_resource type=""PackedScene"" path=""res://Vehicles/space_fighter_3w_lights.tscn"" id = ""space_fighter""]");
+            //final.AppendLine(@"[ext_resource type=""PackedScene"" path=""res://Vehicles/cycle_2.tscn"" id = ""cycle""]");
+            //final.AppendLine(@"[ext_resource type=""PackedScene"" path=""res://Vehicles/propeller_pack_2.tscn"" id = ""prop_pack""]");
+            //final.AppendLine(@"[ext_resource type=""PackedScene"" path=""res://player.tscn"" id=""player""]");
+            //final.AppendLine(@"[ext_resource type=""PackedScene"" path=""res://world.tscn"" id=""world""]");
 
             // BUILDER RESOURCES (ALL of them together)
             final.Append(cubeBuilder.ResourceSB);
@@ -48,7 +48,7 @@ namespace MazeGenerator
             final.AppendLine();
 
             // ROOT NODE
-            final.AppendLine(@"[node name=""Labyrinth3D"" type=""Node3D""]");
+            final.AppendLine(@"[node name=""Generated"" type=""Node3D""]");
             final.AppendLine();
 
             // 1. CALCULATE THE TOP OF THE MAZE
@@ -56,48 +56,48 @@ namespace MazeGenerator
             float roofY = CubeDimensions.Height * CubeDimensions.VerticalSize;
 
             // 2. DEFINE SPAWN MARKERS
-            // We use your existing height variables and ratios but turn them into Markers
-            var spawnConfig = new[] {
-    (Name: "PlayerSpawn", XRatio: 3.0f, ZRatio: 2.0f, HeightOffset: 0.5f), // Dropped from 1.0
-    (Name: "PropPackSpawn", XRatio: 3.0f, ZRatio: 3.0f, HeightOffset: 0.5f), // Dropped from 1.25
-    (Name: "CycleSpawn", XRatio: 3.0f, ZRatio: 4.0f, HeightOffset: 0.5f),    // Dropped from 1.5
-    (Name: "SpaceFighterSpawn", XRatio: 3.0f, ZRatio: 6.0f, HeightOffset: 1.0f) // Dropped from 7.0!
-};
+//            // We use your existing height variables and ratios but turn them into Markers
+//            var spawnConfig = new[] {
+//    (Name: "PlayerSpawn", XRatio: 3.0f, ZRatio: 2.0f, HeightOffset: 0.5f), // Dropped from 1.0
+//    (Name: "PropPackSpawn", XRatio: 3.0f, ZRatio: 3.0f, HeightOffset: 0.5f), // Dropped from 1.25
+//    (Name: "CycleSpawn", XRatio: 3.0f, ZRatio: 4.0f, HeightOffset: 0.5f),    // Dropped from 1.5
+//    (Name: "SpaceFighterSpawn", XRatio: 3.0f, ZRatio: 6.0f, HeightOffset: 1.0f) // Dropped from 7.0!
+//};
 
-            foreach (var spawn in spawnConfig)
-            {
-                float x = (CubeDimensions.Width * CubeDimensions.HorizontalSize) / spawn.XRatio;
-                float z = (CubeDimensions.Length * CubeDimensions.HorizontalSize) / spawn.ZRatio;
-                float y = roofY + spawn.HeightOffset;
+//            foreach (var spawn in spawnConfig)
+//            {
+//                float x = (CubeDimensions.Width * CubeDimensions.HorizontalSize) / spawn.XRatio;
+//                float z = (CubeDimensions.Length * CubeDimensions.HorizontalSize) / spawn.ZRatio;
+//                float y = roofY + spawn.HeightOffset;
 
-                final.AppendLine($@"[node name=""{spawn.Name}"" type=""Marker3D"" parent="".""]");
-                final.AppendLine($"transform = Transform3D(1, 0, 0, 0, 1, 0, 0, 0, 1, {x}, {y}, {z})");
-                final.AppendLine();
-            }
+//                final.AppendLine($@"[node name=""{spawn.Name}"" type=""Marker3D"" parent="".""]");
+//                final.AppendLine($"transform = Transform3D(1, 0, 0, 0, 1, 0, 0, 0, 1, {x}, {y}, {z})");
+//                final.AppendLine();
+//            }
 
-            // 1. Add the Player node
-            final.AppendLine(@"[node name=""Player"" parent=""."" instance=ExtResource(""player"")]");
-            final.AppendLine();
+//            // 1. Add the Player node
+//            final.AppendLine(@"[node name=""Player"" parent=""."" instance=ExtResource(""player"")]");
+//            final.AppendLine();
 
-            // 2. Add the Vehicle nodes (using the names expected by GameManager)
-            final.AppendLine(@"[node name=""Cycle"" parent=""."" instance=ExtResource(""cycle"")]");
-            final.AppendLine();
+//            // 2. Add the Vehicle nodes (using the names expected by GameManager)
+//            final.AppendLine(@"[node name=""Cycle"" parent=""."" instance=ExtResource(""cycle"")]");
+//            final.AppendLine();
 
-            final.AppendLine(@"[node name=""SpaceFighter"" parent=""."" instance=ExtResource(""space_fighter"")]");
-            final.AppendLine();
+//            final.AppendLine(@"[node name=""SpaceFighter"" parent=""."" instance=ExtResource(""space_fighter"")]");
+//            final.AppendLine();
 
-            final.AppendLine(@"[node name=""PropellerPack"" parent=""."" instance=ExtResource(""prop_pack"")]");
-            final.AppendLine();
-            // WORLD node
-            final.AppendLine(@"[node name=""Environment"" parent=""."" instance=ExtResource(""world"")]");
-            final.AppendLine();
+//            final.AppendLine(@"[node name=""PropellerPack"" parent=""."" instance=ExtResource(""prop_pack"")]");
+//            final.AppendLine();
+//            // WORLD node
+//            final.AppendLine(@"[node name=""Environment"" parent=""."" instance=ExtResource(""world"")]");
+//            final.AppendLine();
 
-            // BUILDER NODES (ALL of them together)
+//            // BUILDER NODES (ALL of them together)
             final.Append(cubeBuilder.NodeSB);
             final.Append(levelBuilder.NodeSB);
 
             // 4. SAVE
-            string gameFileName = "../../../Labyrinth3D2.tscn";
+            string gameFileName = "../../../Generated.tscn";
 
             try
             {
